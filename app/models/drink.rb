a0f69f308717d8bd8a6e0aa7334c55a75fcb8506
kaz01267 <k29111776.prog@gmail.com>
2026-02-13 19:14:46 +0900

    Drinks CRUD と認証機能を実装

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -2,5 +2,5 @@ class Drink < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :stock_ml, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_ml, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
