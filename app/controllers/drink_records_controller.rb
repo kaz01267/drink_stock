7293f2b6b4ce51642ed0de7bd66866743ce08479
kaz01267 <k29111776.prog@gmail.com>
2026-02-17 19:59:27 +0900

    飲酒記録編集機能追加と在庫バリデーション修正

--- app/controllers/drink_records_controller.rb
+++ app/controllers/drink_records_controller.rb
@@ -1,6 +1,7 @@
 class DrinkRecordsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_drink
+  before_action :set_drink_record, only: %i[edit update destroy]
 
   def new
     @drink_record = @drink.drink_records.new(consumed_at: Time.current)
@@ -32,10 +33,54 @@ class DrinkRecordsController < ApplicationController
     render :new, status: :unprocessable_entity
   end
 
+  def edit
+  end
+
+  def update
+    new_consumed = drink_record_params[:consumed_ml].to_i
+
+    ActiveRecord::Base.transaction do
+      @drink.lock!
+
+      old_consumed = @drink_record.consumed_ml_in_database || 0
+      restored_stock = @drink.stock_ml + old_consumed
+      new_stock = restored_stock - new_consumed
+
+      if new_stock < 0
+        @drink_record.assign_attributes(drink_record_params) # 画面に入力値を残す
+        @drink_record.errors.add(:consumed_ml, "が在庫を超えています")
+        raise ActiveRecord::RecordInvalid, @drink_record
+      end
+
+      @drink_record.update!(drink_record_params)
+      @drink.update!(stock_ml: new_stock)
+    end
+
+    redirect_to drink_path(@drink), notice: "飲酒記録を更新しました"
+  rescue ActiveRecord::RecordInvalid
+    render :edit, status: :unprocessable_entity
+  end
+
+  def destroy
+    ActiveRecord::Base.transaction do
+      @drink.lock!
+
+      # 削除する分、在庫を戻す
+      @drink.update!(stock_ml: @drink.stock_ml + @drink_record.consumed_ml)
+      @drink_record.destroy!
+    end
+
+    redirect_to drink_path(@drink), notice: "飲酒記録を削除しました"
+  end
+
   private
 
   def set_drink
     @drink = current_user.drinks.find(params[:drink_id])
+  end
+
+  def set_drink_record
+    @drink_record = @drink.drink_records.find(params[:id])
   end
 
   def drink_record_params
--- test/models/drink_record_test.rb
+++ test/models/drink_record_test.rb
@@ -5,16 +5,17 @@ class DrinkRecordTest < ActiveSupport::TestCase
     user  = users(:one)
     drink = user.drinks.create!(name: "テスト酒", stock_ml: 100)
 
+    record = user.drink_records.new(
+      drink: drink,
+      consumed_ml: 200,
+      consumed_at: Time.current
+    )
+
     assert_no_difference "DrinkRecord.count" do
-      record = user.drink_records.new(
-        drink: drink,
-        consumed_ml: 200,
-        consumed_at: Time.current
-      )
-      assert_not record.valid?
-      assert_includes record.errors[:consumed_ml], "が在庫を超えています"
+      assert_not record.save
     end
 
+    assert_includes record.errors[:consumed_ml], "が在庫を超えています"
     assert_equal 100, drink.reload.stock_ml
   end
 end
