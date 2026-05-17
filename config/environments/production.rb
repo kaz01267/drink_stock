3c2f779cecb5a4f3e40fa9f3b6a67e23ce814a9a
kaz01267 <k29111776.prog@gmail.com>
2026-05-17 19:55:03 +0900

    本番メール送信設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,18 @@ Rails.application.configure do
     host: "drink-stock.onrender.com",
     protocol: "https"
   }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
