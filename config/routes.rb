8fb0acdb073492d1901881e3dd33d19ab5549135
kaz01267 <k29111776.prog@gmail.com>
2026-05-28 00:21:01 +0900

    ユーザーネーム機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,18 @@ Rails.application.routes.draw do
   get "terms",   to: "pages#terms"
   get "privacy", to: "pages#privacy"
   get "guide", to: "pages#guide"
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+
   resources :drinks do
     resources :drink_records, only: %i[new create edit update destroy]
   end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
   root to: "pages#home"
 end
