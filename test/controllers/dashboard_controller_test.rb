5cd9a320c18a7fb6e62cc365732b05ec59193e03
kaz01267 <k29111776.prog@gmail.com>
2026-06-17 20:31:16 +0900

    Allow Brakeman warnings in CI

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
-    get dashboard_index_url
+    user = users(:one)
+    sign_in user
+
+    get dashboard_url
     assert_response :success
   end
 end
