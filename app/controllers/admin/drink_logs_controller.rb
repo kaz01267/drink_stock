b303a89f6fa2085b5242c73f918964d67161baef
kaz01267 <k29111776.prog@gmail.com>
2026-08-03 22:54:20 +0900

    管理者お酒ログ一覧に検索機能を追加

--- app/controllers/admin/drink_logs_controller.rb
+++ app/controllers/admin/drink_logs_controller.rb
@@ -1,5 +1,15 @@
 class Admin::DrinkLogsController < Admin::BaseController
   def index
-    @drink_logs = DrinkLog.includes(:user, :drink).order(logged_at: :desc)
+    @drink_logs = DrinkLog
+                    .left_joins(:user, :drink)
+                    .includes(:user, :drink)
+                    .order(logged_at: :desc)
+
+    if params[:q].present?
+      @drink_logs = @drink_logs.where(
+        "drink_logs.drink_name ILIKE :q OR drinks.name ILIKE :q OR drink_logs.memo ILIKE :q OR users.name ILIKE :q OR users.email ILIKE :q",
+        q: "%#{params[:q]}%"
+      )
+    end
   end
 end
